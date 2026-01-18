8c6768f8119cd44986c763d265adf19952d9dc0f
Angel Perez <perryplatipus4@gmail.com>
2026-01-17 20:04:29 -0800

    Lel

---
+++ Lab 00/bookType/bookType.h
@@ -0,0 +1,30 @@
+#include <string>
+using namespace std;
+
+class bookType {
+    public:
+        bookType();
+        void setTitle(string BookName);
+        string getTitle();
+        void setStock(int amount);
+        int getStock();
+        void addAuthor(int amount);
+        void getAuthor();
+        void setPublisher(string pub);
+        string getPublisher();
+        void setisbn(int bookNum);
+        int getisbn();
+        void setPrice(double cost);
+        double getPrice();
+        void setPublicationYear(int year);
+        int getPublicationYear();
+    private:
+        string title;
+        string authors[4];
+        int numAuthors;
+        string publisher;
+        int isbn;
+        double price;
+        int publicationYear;
+        int stock;
+};
---
+++ Lab 00/bookType/bookTypeImp.cpp
@@ -0,0 +1,21 @@
+#include "bookType.h"
+
+void bookType::setTitle(string BookName){
+    
+};
+
+string bookType::getTitle(){
+
+};
+    void setStock(int amount);
+    int getStock();
+        void addAuthor(int amount);
+        void getAuthor();
+        void setPublisher(string pub);
+        string getPublisher();
+        void setisbn(int bookNum);
+        int getisbn();
+        void setPrice(double cost);
+        double getPrice();
+        void setPublicationYear(int year);
+        int getPublicationYear();
