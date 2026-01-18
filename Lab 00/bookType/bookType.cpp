1116f3905ed46282afdb267b266eed4191ac02de
Angel Perez <perryplatipus4@gmail.com>
2026-01-18 01:38:05 -0800

    Update bookType implementation: initialize Book2 with parameters and call getTitle

--- Lab 00/bookType/bookType.cpp
+++ Lab 00/bookType/bookType.cpp
@@ -1,7 +1,10 @@
 #include "bookType.h"
+#include "bookTypeImp.cpp"
 using namespace std;
 
+
 int main() {
-    bookType Book1, Book2;
-    
+    bookType Book1, Book2("The GOAT", 2, "Angel", "GOAT Productions", 12345, 14.99, 2006);
+
+    Book2.getTitle();
 }
