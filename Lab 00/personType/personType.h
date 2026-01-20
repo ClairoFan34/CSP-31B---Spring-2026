be300d0869bcb56632c43f28cbc7c9edd2b7af37
Angel Perez <perryplatipus4@gmail.com>
2026-01-20 11:49:57 -0800

    Add personType class implementation and test program

---
+++ Lab 00/personType/personType.h
@@ -0,0 +1,46 @@
+//personType.h
+    
+#include <string> 
+
+using namespace std;
+
+class personType
+{
+public:
+    void print() const;
+       //Function to output the first name and last name
+       //in the form firstName lastName.
+  
+    void setName(string first, string last);
+      //Function to set firstName and lastName according 
+      //to the parameters.
+      //Postcondition: firstName = first; lastName = last
+
+    string getFirstName() const;
+      //Function to return the first name.
+      //Postcondition: The value of firstName is returned.
+
+    string getLastName() const;
+      //Function to return the last name.
+      //Postcondition: The value of lastName is returned.
+
+    personType(string first = "", string last = "");
+      //Constructor
+      //Sets firstName and lastName according to the parameters.
+      //The default values of the parameters are null strings.
+      //Postcondition: firstName = first; lastName = last  
+
+    void setFirstName (string first);
+
+    void setLastName (string last);
+
+    void setMiddleName (string middle);
+
+    bool checkName (personType name);  
+
+ private:
+    string firstName; //variable to store the first name
+    string lastName;  //variable to store the last name
+    string middleName;
+};
+
